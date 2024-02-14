d4302aa15327c4abf972bf9979761c7cd7a476c5
David <acostafajardodavid@gmail.com>
2024-02-14 15:29:57 -0700

    Correccion de clases

--- BancoPrestacion/src/test/java/test/TestCrearCliente.java
+++ BancoPrestacion/src/test/java/test/TestCrearCliente.java
@@ -1,3 +1,7 @@
+package test;
+
+import java.util.Date;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -8,5 +12,6 @@
  * @author af_da
  */
 public class TestCrearCliente {
-    
+    Cliente cliente = new Cliente("usuario123", "password123", "Juan", "Perez", "Gomez", new Date(90, 4, 15), 34);
+
 }
