7b6f898eff9a2b3d65bc96ecd02d39af8887653f
Nube28 <alejandro.gomez247313@potros.itson.edu.mx>
2024-02-13 21:19:34 -0700

    Update BancoPersistencia.java

--- BancoPersistencia/src/main/java/org/itson/bdavanzadas/bancopersistencia/BancoPersistencia.java
+++ BancoPersistencia/src/main/java/org/itson/bdavanzadas/bancopersistencia/BancoPersistencia.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package org.itson.bdavanzadas.bancopersistencia;
 
-/**
- *
- * @author Berry
- */
 public class BancoPersistencia {
 
     public static void main(String[] args) {
