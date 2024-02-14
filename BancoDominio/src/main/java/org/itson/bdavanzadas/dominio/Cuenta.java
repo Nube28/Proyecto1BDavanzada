453211d67bec3af3716aad4ac119e9798e69a9de
Nube28 <alejandro.gomez247313@potros.itson.edu.mx>
2024-02-13 22:48:12 -0700

    Clase domicilio y cuenta

--- BancoDominio/src/main/java/org/itson/bdavanzadas/dominio/Cuenta.java
+++ BancoDominio/src/main/java/org/itson/bdavanzadas/dominio/Cuenta.java
@@ -1,5 +1,139 @@
 package org.itson.bdavanzadas.dominio;
 
+import java.util.Date;
+import java.util.Objects;
+
 public class Cuenta {
+    private int id_cuenta;
+    private Date fecha_apertura;
+    private int numero;
+    private float saldo;
+    private int id_transaccion;
+    private int id_cliente;
+
+    public Cuenta(int id_cuenta, Date fecha_apertura, int numero, float saldo, int id_transaccion, int id_cliente) {
+        this.id_cuenta = id_cuenta;
+        this.fecha_apertura = fecha_apertura;
+        this.numero = numero;
+        this.saldo = saldo;
+        this.id_transaccion = id_transaccion;
+        this.id_cliente = id_cliente;
+    }
+
+    public Cuenta(Date fecha_apertura, int numero, float saldo, int id_transaccion, int id_cliente) {
+        this.fecha_apertura = fecha_apertura;
+        this.numero = numero;
+        this.saldo = saldo;
+        this.id_transaccion = id_transaccion;
+        this.id_cliente = id_cliente;
+    }
+
+    public Cuenta() {
+    }
+    
+    public int getId_cuenta() {
+        return id_cuenta;
+    }
+
+    public void setId_cuenta(int id_cuenta) {
+        this.id_cuenta = id_cuenta;
+    }
+
+    public Date getFecha_apertura() {
+        return fecha_apertura;
+    }
+
+    public void setFecha_apertura(Date fecha_apertura) {
+        this.fecha_apertura = fecha_apertura;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public float getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(float saldo) {
+        this.saldo = saldo;
+    }
+
+    public int getId_transaccion() {
+        return id_transaccion;
+    }
+
+    public void setId_transaccion(int id_transaccion) {
+        this.id_transaccion = id_transaccion;
+    }
+
+    public int getId_cliente() {
+        return id_cliente;
+    }
+
+    public void setId_cliente(int id_cliente) {
+        this.id_cliente = id_cliente;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 67 * hash + this.id_cuenta;
+        hash = 67 * hash + Objects.hashCode(this.fecha_apertura);
+        hash = 67 * hash + this.numero;
+        hash = 67 * hash + Float.floatToIntBits(this.saldo);
+        hash = 67 * hash + this.id_transaccion;
+        hash = 67 * hash + this.id_cliente;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Cuenta other = (Cuenta) obj;
+        if (this.id_cuenta != other.id_cuenta) {
+            return false;
+        }
+        if (this.numero != other.numero) {
+            return false;
+        }
+        if (Float.floatToIntBits(this.saldo) != Float.floatToIntBits(other.saldo)) {
+            return false;
+        }
+        if (this.id_transaccion != other.id_transaccion) {
+            return false;
+        }
+        if (this.id_cliente != other.id_cliente) {
+            return false;
+        }
+        return Objects.equals(this.fecha_apertura, other.fecha_apertura);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Cuenta{");
+        sb.append("id_cuenta=").append(id_cuenta);
+        sb.append(", fecha_apertura=").append(fecha_apertura);
+        sb.append(", numero=").append(numero);
+        sb.append(", saldo=").append(saldo);
+        sb.append(", id_transaccion=").append(id_transaccion);
+        sb.append(", id_cliente=").append(id_cliente);
+        sb.append('}');
+        return sb.toString();
+    }
+    
     
 }
--- BancoDominio/src/main/java/org/itson/bdavanzadas/dominio/Domicilio.java
+++ BancoDominio/src/main/java/org/itson/bdavanzadas/dominio/Domicilio.java
@@ -1,5 +1,154 @@
 package org.itson.bdavanzadas.dominio;
 
+import java.util.Objects;
+
 public class Domicilio {
+    private int id_domicilio;
+    private String calle;
+    private int numero_exterior;
+    private int numero_interioro;
+    private int contrasenia;
+    private String codigo_postal;
+    private String colonia;
+
+    public Domicilio() {
+    }
+
+    public Domicilio(int id_domicilio, String calle, int numero_exterior, int numero_interioro, int contrasenia, String codigo_postal, String colonia) {
+        this.id_domicilio = id_domicilio;
+        this.calle = calle;
+        this.numero_exterior = numero_exterior;
+        this.numero_interioro = numero_interioro;
+        this.contrasenia = contrasenia;
+        this.codigo_postal = codigo_postal;
+        this.colonia = colonia;
+    }
+
+    public Domicilio(String calle, int numero_exterior, int numero_interioro, int contrasenia, String codigo_postal, String colonia) {
+        this.calle = calle;
+        this.numero_exterior = numero_exterior;
+        this.numero_interioro = numero_interioro;
+        this.contrasenia = contrasenia;
+        this.codigo_postal = codigo_postal;
+        this.colonia = colonia;
+    }
+
+    public int getId_domicilio() {
+        return id_domicilio;
+    }
+
+    public void setId_domicilio(int id_domicilio) {
+        this.id_domicilio = id_domicilio;
+    }
+
+    public String getCalle() {
+        return calle;
+    }
+
+    public void setCalle(String calle) {
+        this.calle = calle;
+    }
+
+    public int getNumero_exterior() {
+        return numero_exterior;
+    }
+
+    public void setNumero_exterior(int numero_exterior) {
+        this.numero_exterior = numero_exterior;
+    }
+
+    public int getNumero_interioro() {
+        return numero_interioro;
+    }
+
+    public void setNumero_interioro(int numero_interioro) {
+        this.numero_interioro = numero_interioro;
+    }
+
+    public int getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setContrasenia(int contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+
+    public String getCodigo_postal() {
+        return codigo_postal;
+    }
+
+    public void setCodigo_postal(String codigo_postal) {
+        this.codigo_postal = codigo_postal;
+    }
+
+    public String getColonia() {
+        return colonia;
+    }
+
+    public void setColonia(String colonia) {
+        this.colonia = colonia;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 29 * hash + this.id_domicilio;
+        hash = 29 * hash + Objects.hashCode(this.calle);
+        hash = 29 * hash + this.numero_exterior;
+        hash = 29 * hash + this.numero_interioro;
+        hash = 29 * hash + this.contrasenia;
+        hash = 29 * hash + Objects.hashCode(this.codigo_postal);
+        hash = 29 * hash + Objects.hashCode(this.colonia);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Domicilio other = (Domicilio) obj;
+        if (this.id_domicilio != other.id_domicilio) {
+            return false;
+        }
+        if (this.numero_exterior != other.numero_exterior) {
+            return false;
+        }
+        if (this.numero_interioro != other.numero_interioro) {
+            return false;
+        }
+        if (this.contrasenia != other.contrasenia) {
+            return false;
+        }
+        if (!Objects.equals(this.calle, other.calle)) {
+            return false;
+        }
+        if (!Objects.equals(this.codigo_postal, other.codigo_postal)) {
+            return false;
+        }
+        return Objects.equals(this.colonia, other.colonia);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Domicilio{");
+        sb.append("id_domicilio=").append(id_domicilio);
+        sb.append(", calle=").append(calle);
+        sb.append(", numero_exterior=").append(numero_exterior);
+        sb.append(", numero_interioro=").append(numero_interioro);
+        sb.append(", contrasenia=").append(contrasenia);
+        sb.append(", codigo_postal=").append(codigo_postal);
+        sb.append(", colonia=").append(colonia);
+        sb.append('}');
+        return sb.toString();
+    }
+    
     
 }
