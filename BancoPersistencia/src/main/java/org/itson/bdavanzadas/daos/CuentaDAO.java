84c5e056b8ea6ac9b00dbee4b9de081b041727fe
Nube28 <alejandro.gomez247313@potros.itson.edu.mx>
2024-02-13 23:01:50 -0700

    Creacion de CuentaDAO/DTO

---
+++ BancoPersistencia/src/main/java/org/itson/bdavanzadas/daos/CuentaDAO.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.itson.bdavanzadas.daos;
+
+import java.util.List;
+import org.itson.bdavanzadas.dtos.CuentaNuevaDTO;
+import org.itson.bdavanzadas.excepciones.PersistenciaException;
+
+/**
+ *
+ * @author Berry
+ */
+public class CuentaDAO implements ICuentaDAO{
+
+    @Override
+    public Object agregar(CuentaNuevaDTO socioNuevo) throws PersistenciaException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public List<Object> consultar() throws PersistenciaException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void eliminar(String ID) throws PersistenciaException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Object actualizar(String ID, CuentaNuevaDTO cuentaNueva) throws PersistenciaException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
---
+++ BancoPersistencia/src/main/java/org/itson/bdavanzadas/daos/ICuentaDAO.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package org.itson.bdavanzadas.daos;
+
+import java.util.List;
+import org.itson.bdavanzadas.dtos.CuentaNuevaDTO;
+import org.itson.bdavanzadas.excepciones.PersistenciaException;
+
+/**
+ *
+ * @author Berry
+ */
+public interface ICuentaDAO {
+    Cuenta agregar(CuentaNuevaDTO socioNuevo) throws PersistenciaException;
+    List<Cuenta> consultar() throws PersistenciaException;
+    void eliminar(String ID) throws PersistenciaException;
+    Cuenta actualizar(String ID, CuentaNuevaDTO cuentaNueva) throws PersistenciaException;
+}
