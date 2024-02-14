6020642934f498a8e7dd611e2971a8aaa662d085
Nube28 <alejandro.gomez247313@potros.itson.edu.mx>
2024-02-13 21:11:08 -0700

    Update BancoPresentacion.java

--- BancoPresentacion/src/main/java/org/itson/bdavanzadas/bancopresentacion/BancoPresentacion.java
+++ BancoPresentacion/src/main/java/org/itson/bdavanzadas/bancopresentacion/BancoPresentacion.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package org.itson.bdavanzadas.bancopresentacion;
 
-/**
- *
- * @author Berry
- */
 public class BancoPresentacion {
 
     public static void main(String[] args) {
