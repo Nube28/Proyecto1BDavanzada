186228a18b04106772059db805ae75df1960bf56
Nube28 <alejandro.gomez247313@potros.itson.edu.mx>
2024-02-13 20:53:31 -0700

    Crear los archivos

---
+++ BancoDominio/src/main/java/org/itson/bdavanzadas/bancodominio/BancoDominio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.itson.bdavanzadas.bancodominio;
+
+/**
+ *
+ * @author Berry
+ */
+public class BancoDominio {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ BancoPresentacion/src/main/java/org/itson/bdavanzadas/bancopresentacion/BancoPresentacion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.itson.bdavanzadas.bancopresentacion;
+
+/**
+ *
+ * @author Berry
+ */
+public class BancoPresentacion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
