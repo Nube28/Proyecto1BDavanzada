8a5c3a4fb074a990b69a3991a7858fa284273b57
Nube28 <alejandro.gomez247313@potros.itson.edu.mx>
2024-02-17 16:43:21 -0700

    cancelar cuenta

--- BancoPresentacion/src/main/java/com/mycompany/bancoprestacion/CancelarCuenta.java
+++ BancoPresentacion/src/main/java/com/mycompany/bancoprestacion/CancelarCuenta.java
@@ -4,11 +4,14 @@
  */
 package com.mycompany.bancoprestacion;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 import org.itson.bdavanzadas.conexion.IConexion;
 import org.itson.bdavanzadas.daos.ICuentaDAO;
 import org.itson.bdavanzadas.dominio.Cliente;
 import org.itson.bdavanzadas.dominio.Cuenta;
+import org.itson.bdavanzadas.excepciones.PersistenciaException;
 
 /**
  *
@@ -268,7 +271,11 @@ public class CancelarCuenta extends javax.swing.JFrame {
 
     private void btnConfirmarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnConfirmarActionPerformed
         if(pasContraseñaUsuario.getText().equals(cliente.getContrasenia())){
-            //cuenta.setActivo(false); Necesitamso eso en la base de datos
+            try {
+                cuentaDAO.CancelarCuenta(cuenta.getId_cuenta());
+            } catch (PersistenciaException ex) {
+                Logger.getLogger(CancelarCuenta.class.getName()).log(Level.SEVERE, null, ex);
+            }
             JOptionPane.showMessageDialog(this, "Se cancelo la cuenta correctamente");
             MenuPrincipal mp = new MenuPrincipal(cliente,cuentaDAO,conexion);
             mp.setVisible(true);
